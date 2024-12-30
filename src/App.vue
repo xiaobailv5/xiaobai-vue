fa59068b4132cef17058965232aff9477d7dcd4a
gxjh <gxjh205@163.com>
2024-12-30 13:17:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import LoginVue from "@/views/Login.vue";
+
+</script>
+
+<template>
+  <LoginVue />
+  
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,24 @@
+import './assets/main.scss'
+
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// import router from '@/router'
+// import {createPinia} from 'pinia'
+// import { createPersistedState } from 'pinia-persistedstate-plugin'
+
+import App from './App.vue'
+
+
+
+const app = createApp(App);
+
+// const pinia = createPinia();
+// const persist = createPersistedState();
+// pinia.use(persist)
+// app.use(pinia);
+app.use(ElementPlus);
+// app.use(router);
+app.mount('#app');
+
+
